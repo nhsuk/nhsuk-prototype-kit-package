23487c84ba48bb9739082af5e9c96c5d51342332
Frankie Roberto <frankie@frankieroberto.com>
2025-10-22 14:37:41 +0100

    Switch to node test assertions

--- tests/express-middleware/production-headers.test.js
+++ tests/express-middleware/production-headers.test.js
@@ -2,30 +2,36 @@ const productionHeaders = require('../../lib/express-middleware/production-heade
 const http = require('http')
 const request = require('supertest')
 const express = require('express');
+const { describe, it } = require('node:test')
+const assert = require('node:assert')
+
 
 const app = express();
 app.use(productionHeaders);
 
 const server = http.createServer(app);
 
-test('productionHeaders adds content security policy', async () => {  
-  const response = await request(server).get('/nonexistent');
-  
-  expect(response.headers['content-security-policy']).toBe('upgrade-insecure-requests')
-});
+describe('productionHeaders', () => {
 
-test('productionHeaders adds Strict-Transport-Security', async () => {  
-  const response = await request(server).get('/nonexistent');
+  it('adds content security policy', async () => {  
+    const response = await request(server).get('/nonexistent');
+    
+    assert.strictEqual(response.headers['content-security-policy'], 'upgrade-insecure-requests')
+  });
   
-  expect(response.headers['strict-transport-security']).toBe('max-age=31536000; includeSubDomains; preload')
-});
-
-test('it redirects from http to https', async () => {    
-  const response = await request(server)
-    .get('/secure-only')
-    .set('Host', 'example.com');
+  it('adds Strict-Transport-Security', async () => {  
+    const response = await request(server).get('/nonexistent');
+    
+    assert.strictEqual(response.headers['strict-transport-security'], 'max-age=31536000; includeSubDomains; preload')
+  });
   
-  expect(response.status).toBe(302)
-  expect(response.headers.location).toBe('https://example.com/secure-only')
-
-});
+  it('redirects from http to https', async () => {    
+    const response = await request(server)
+      .get('/secure-only')
+      .set('Host', 'example.com');
+    
+    assert.strictEqual(response.status, 302)
+    assert.strictEqual(response.headers.location, 'https://example.com/secure-only')
+  
+  });
+})
