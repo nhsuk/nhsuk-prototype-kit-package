4c5cd872cf647d2f14f17dac33f8e3a8aff2bb77
Frankie Roberto <frankie@frankieroberto.com>
2025-10-22 14:04:18 +0100

    Switch to Node test runner (from Jest)

--- tests/nunjucks-filters/format-nhs-number.test.js
+++ tests/nunjucks-filters/format-nhs-number.test.js
@@ -1,29 +1,35 @@
+const { describe, it } = require('node:test')
+const assert = require('node:assert')
 const formatNhsNumber = require('../../lib/nunjucks-filters/format-nhs-number')
 
-test('formatNhsNumber with a string of 10 digits', () => {
-  expect(formatNhsNumber('9991231230')).toBe('999 123 1230')
-})
 
-test('formatNhsNumber with 10 digits as a number', () => {
-  expect(formatNhsNumber(9991231230)).toBe('999 123 1230')
-})
+describe('formatNhsNumber', () => {
+  it('should format a string of 10 digits', () => {
+    assert.strictEqual(formatNhsNumber('9991231230'), '999 123 1230')
+  })
+  
+  it('should format 10 digits as a number', () => {
+    assert.strictEqual(formatNhsNumber(9991231230), '999 123 1230')
+  })
+  
+  it('should format a string of 10 digits containing spaces', () => {
+    assert.strictEqual(formatNhsNumber('9 991 23 123 0  '), '999 123 1230')
+  })
+  
+  it('should format a string of 10 digits and other characters', () => {
+    assert.strictEqual(formatNhsNumber('999-123-123-0'), '999 123 1230')
+  })
+  
+  it('should not format a string of less than 10 digits', () => {
+    assert.strictEqual(formatNhsNumber('99912345'), '99912345')
+  })
+  
+  it('should not format a string of more than 10 digits', () => {
+    assert.strictEqual(formatNhsNumber('9991234567123'), '9991234567123')
+  })
+  
+  it('should return null for null', () => {
+    assert.strictEqual(formatNhsNumber(null), null)
+  })
 
-test('formatNhsNumber with a string of 10 digits containing spaces', () => {
-  expect(formatNhsNumber('9 991 23 123 0  ')).toBe('999 123 1230')
-})
-
-test('formatNhsNumber with a string of 10 digits and other characters', () => {
-  expect(formatNhsNumber('999-123-123-0')).toBe('999 123 1230')
-})
-
-test('formatNhsNumber with a string of less than 10 digits', () => {
-  expect(formatNhsNumber('99912345')).toBe('99912345')
-})
-
-test('formatNhsNumber with a string of more than 10 digits', () => {
-  expect(formatNhsNumber('9991234567123')).toBe('9991234567123')
-})
-
-test('formatNhsNumber with null', () => {
-  expect(formatNhsNumber(null)).toBeNull()
 })
