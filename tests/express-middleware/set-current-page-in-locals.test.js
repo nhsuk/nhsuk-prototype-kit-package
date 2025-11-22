5b5ee38fe4dd78552b3b5fcff0556768965358d2
Frankie Roberto <frankie@frankieroberto.com>
2025-11-22 22:37:22 +0000

    Add tests for setting URL path in locals

---
+++ tests/express-middleware/set-current-page-in-locals.test.js
@@ -0,0 +1,39 @@
+const assert = require('node:assert')
+const { describe, it, beforeEach } = require('node:test')
+
+const setCurrentPageInLocals = require('../../lib/express-middleware/set-current-page-in-locals')
+
+describe('setCurrentPageInLocals middleware', () => {
+  let req, res, nextCalled
+
+  beforeEach(() => {
+    nextCalled = false
+    res = {
+      locals: {}
+    }
+  })
+
+  const next = () => {
+    nextCalled = true
+  }
+
+  it('should set the original path including querystring in locals', () => {
+    req = {
+      originalUrl: '/this/is/a/test?query=string',
+      path: '/this/is/a/test'
+    }
+
+    setCurrentPageInLocals(req, res, next)
+
+    assert.deepStrictEqual(
+      res.locals.currentPage,
+      '/this/is/a/test?query=string'
+    )
+  })
+
+  it('should call next()', () => {
+    setCurrentPageInLocals(req, res, next)
+
+    assert.strictEqual(nextCalled, true)
+  })
+})
