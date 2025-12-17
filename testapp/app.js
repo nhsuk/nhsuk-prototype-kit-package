90ec25e53c16986e30048631191d25f00e3c7223
Frankie Roberto <frankie@frankieroberto.com>
2025-12-17 22:40:39 +0000

    Move express and nunjucks setup into kit

--- testapp/app.js
+++ testapp/app.js
@@ -1,32 +1,23 @@
 import { join } from 'node:path'
 
-import express from 'express'
 import NHSPrototypeKit from 'nhsuk-prototype-kit'
-import nunjucks from 'nunjucks'
 
 import sessionDataDefaults from './data/session-data-defaults.js'
 import locals from './locals.js'
 import routes from './routes.js'
 
-const app = express()
 let port = 3000
 
-// Nunjucks configuration for application
-const appViews = [join(import.meta.dirname, 'views')]
-
-const nunjucksAppEnv = nunjucks.configure(appViews, {
-  express: app,
-  noCache: true
-})
+// Views folder for templates
+const appViews = join(import.meta.dirname, 'views')
 
 const prototype = NHSPrototypeKit.init({
   serviceName: 'Test service',
-  express: app,
-  nunjucks: nunjucksAppEnv,
   buildOptions: {
     entryPoints: ['assets/sass/*.scss', 'assets/javascript/*.js'],
     sassLoadPaths: ['../node_modules']
   },
+  appViews,
   routes,
   locals,
   sessionDataDefaults
