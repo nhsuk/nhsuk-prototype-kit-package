6af4f350325272de82e4c265fb12f7ba0719fa5b
Frankie Roberto <frankie.roberto1@nhs.net>
2025-12-18 21:35:36 +0000

    Move nunjucks setup to package (#138)
    This moves the setup of nunjucks load paths to use the 4 views folders
    in `nhsuk-frontend` from `app.js` to within the package - aiming to
    reduce the amount of boilerplate code in `app.js`.

--- testapp/app.js
+++ testapp/app.js
@@ -1,44 +1,23 @@
-import { dirname, join } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { join } from 'node:path'
 
-import express from 'express'
 import NHSPrototypeKit from 'nhsuk-prototype-kit'
-import nunjucks from 'nunjucks'
 
 import sessionDataDefaults from './data/session-data-defaults.js'
 import locals from './locals.js'
 import routes from './routes.js'
 
-const app = express()
 let port = 3000
 
-// NHS.UK frontend package path
-const frontendPath = fileURLToPath(
-  dirname(import.meta.resolve('nhsuk-frontend/package.json'))
-)
-
-// Nunjucks configuration for application
-const appViews = [
-  join(import.meta.dirname, 'views'),
-  join(frontendPath, 'dist/nhsuk/components'),
-  join(frontendPath, 'dist/nhsuk/macros'),
-  join(frontendPath, 'dist/nhsuk'),
-  join(frontendPath, 'dist')
-]
-
-const nunjucksAppEnv = nunjucks.configure(appViews, {
-  express: app,
-  noCache: true
-})
+// Views folder for templates
+const viewsPath = join(import.meta.dirname, 'views')
 
 const prototype = NHSPrototypeKit.init({
   serviceName: 'Test service',
-  express: app,
-  nunjucks: nunjucksAppEnv,
   buildOptions: {
     entryPoints: ['assets/sass/*.scss', 'assets/javascript/*.js'],
     sassLoadPaths: ['../node_modules']
   },
+  viewsPath,
   routes,
   locals,
   sessionDataDefaults
