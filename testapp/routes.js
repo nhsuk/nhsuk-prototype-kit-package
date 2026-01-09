43fb7e6556f28f3d6e9f8cdfb1cc954f675937b7
Frankie Roberto <frankie.roberto1@nhs.net>
2026-01-09 15:36:15 +0000

    Add express-flash as default middleware (#160)
    This can be used for "flash" messages like notification banners, which
    only appear once on the next page load.

--- testapp/routes.js
+++ testapp/routes.js
@@ -6,4 +6,9 @@ router.get('/custom-route', (req, res) => {
   res.render('custom')
 })
 
+router.get('/test-flash', (req, res) => {
+  req.flash('success', 'It worked.')
+  res.redirect('/')
+})
+
 export default router
