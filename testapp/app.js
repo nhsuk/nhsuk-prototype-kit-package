7391bd7e4ebce37d26d1c9e1e1640de926a3493a
Colin Rotherham <work@colinr.com>
2026-01-28 18:02:49 +0000

    Add config to test app

--- testapp/app.js
+++ testapp/app.js
@@ -1,25 +1,27 @@
 import NHSPrototypeKit from 'nhsuk-prototype-kit'
 
+import * as config from './app/config.js'
 import { sessionDataDefaults } from './app/data/session-data-defaults.js'
 import * as filters from './app/filters.js'
 import { locals } from './app/locals.js'
 import { routes } from './app/routes.js'
 
-// Views folder for templates
-const viewsPath = ['app/views']
-
 const prototype = await NHSPrototypeKit.init({
-  serviceName: 'Test service',
+  serviceName: config.serviceName,
   buildOptions: {
     entryPoints: ['app/assets/sass/*.scss', 'app/assets/javascript/*.js'],
     sassLoadPaths: ['../node_modules']
   },
-  viewsPath,
+  viewsPath: ['app/views'],
   routes,
   locals,
   sessionDataDefaults
 })
 
+// Add custom port number
+prototype.app?.set('port', config.port)
+
+// Add custom Nunjucks filters
 for (const [name, filter] of Object.entries(filters)) {
   prototype.nunjucks?.addFilter(name, filter)
 }
