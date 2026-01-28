888d8017dbd47f35884579ba0e2e847c708c5d4c
Colin Rotherham <work@colinr.com>
2026-01-28 16:52:02 +0000

    Fix Node.js "ReferenceError: Cannot determine intended module"

--- testapp/app.js
+++ testapp/app.js
@@ -2,9 +2,9 @@ import { join } from 'node:path'
 
 import NHSPrototypeKit from 'nhsuk-prototype-kit'
 
-import sessionDataDefaults from './app/data/session-data-defaults.js'
-import locals from './app/locals.js'
-import routes from './app/routes.js'
+import { sessionDataDefaults } from './app/data/session-data-defaults.js'
+import { locals } from './app/locals.js'
+import { routes } from './app/routes.js'
 
 // Views folder for templates
 const viewsPath = join(import.meta.dirname, 'app/views')
