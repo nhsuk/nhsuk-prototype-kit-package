ff6dff161738ec41123df0a6552c6fe826476d0c
Colin Rotherham <work@colinr.com>
2026-01-29 14:17:30 +0000

    Remove unnecessary `DOMContentLoaded` listener

--- testapp/app/assets/javascript/application.js
+++ testapp/app/assets/javascript/application.js
@@ -1,6 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const appTest = document.querySelector('[data-module="app-test"]')
-  if (appTest) {
-    appTest.textContent = '✅'
-  }
-})
+const $appTest = document.querySelector('[data-module="app-test"]')
+
+if ($appTest) {
+  $appTest.textContent = '✅'
+}
